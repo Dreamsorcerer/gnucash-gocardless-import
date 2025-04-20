8da0636f8dfcc9450f36912f447f8e5d841ec49f
Sam Bull <git@sambull.org>
2025-04-20 15:16:57 +0100

    Add typing stubs

---
+++ stubs/gnucash/__init__.pyi
@@ -0,0 +1,107 @@
+from datetime import date, datetime
+from types import TracebackType
+from typing import Literal, Self
+
+class GncNumeric:
+    def __init__(self, num: float):
+        ...
+
+    def to_double(self) -> float:
+        ...
+
+class GncCommodity:
+    ...
+
+class GncCommodityTable:
+    def lookup(self, commodity_type: Literal["CURRENCY"], symbol: str) -> GncCommodity:
+        ...
+
+class Account:
+    def GetBalanceAsOfDate(self, d: datetime) -> GncNumeric:
+        ...
+
+    def GetSplitList(self) -> list[Split]:
+        ...
+
+    def lookup_by_full_name(self, name: str) -> Account:
+        ...
+
+class Book:
+    def get_root_account(self) -> Account:
+        ...
+
+    def get_table(self) -> GncCommodityTable:
+        ...
+
+class Session:
+    book: Book
+
+    def __init__(self, path: str):
+        ...
+
+    def __enter__(self) -> Self:
+        ...
+
+    def __exit__(self, exc_type: type[BaseException] | None, exc_val: BaseException | None, exc_tb: TracebackType | None) -> None:
+        ...
+
+class Transaction:
+    def __init__(self, book: Book):
+        ...
+
+    def BeginEdit(self) -> None:
+        ...
+
+    def CommitEdit(self) -> None:
+        ...
+
+    def GetDate(self) -> date:
+        ...
+
+    def GetDescription(self) -> str:
+        ...
+
+    def GetSplitList(self) -> list[Split]:
+        ...
+
+    def SetCurrency(self, curr: GncCommodity) -> None:
+        ...
+
+    def SetDate(self, day: int, month: int, year: int) -> None:
+        ...
+
+    def SetDescription(self, desc: str) -> None:
+        ...
+
+class Split:
+    parent: Transaction
+
+    def __init__(self, book: Book):
+        ...
+
+    def GetAccount(self) -> Account:
+        ...
+
+    def GetAmount(self) -> GncNumeric:
+        ...
+
+    def GetMemo(self) -> str:
+        ...
+
+    def GetValue(self) -> GncNumeric:
+        ...
+
+    def SetAccount(self, account: Account) -> None:
+        ...
+
+    def SetMemo(self, memo: str) -> None:
+        ...
+
+    def SetParent(self, parent: Transaction) -> None:
+        ...
+
+    def SetReconcile(self, state: Literal["y", "n", "c"]) -> None:
+        ...
+
+    def SetValue(self, value: GncNumeric) -> None:
+        ...
